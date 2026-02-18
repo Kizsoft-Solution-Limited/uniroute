f24568738ae1aca5e40299546ce3702d9fc5fd6d
Kizsoft Solutions <adikekizinho@gmail.com>
2026-02-18 23:43:37 +0100

    websocket https set

--- cmd/cli/main.go
+++ cmd/cli/main.go
@@ -2,11 +2,16 @@ package main
 
 import (
 	"os"
+	"runtime/debug"
+	"strings"
 
 	"github.com/Kizsoft-Solution-Limited/uniroute/cmd/cli/commands"
 )
 
 func main() {
+	if info, ok := debug.ReadBuildInfo(); ok && info.Main.Version != "" && info.Main.Version != "(devel)" {
+		commands.SetVersion(strings.TrimPrefix(info.Main.Version, "v"))
+	}
 	if err := commands.Execute(); err != nil {
 		os.Exit(1)
 	}
